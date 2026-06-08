2f696c00626826d8bfa369a954f6bb68f1bfc8b1
ZZYandZY <18827561151@163.com>
2026-06-08 12:51:54 +0800

    feat: 前端路由配置及整体框架（Closes #15）

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home-view">
+    <HomeBackground />
+    <Home />
+  </div>
+</template>
+
+<script setup lang="ts">
+import HomeBackground from '../components/HomeBackground.vue'
+import Home from '../components/Home.vue'
+</script>
+
+<style scoped>
+.home-view {
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+}
+</style>
